5097444c4529f65384b0085d5d5480795d6762c3
Ashu2osh0112 <akspwei@gmail.com>
2025-12-04 20:16:10 +0530

    Updating the data.py

--- data.py
+++ data.py
@@ -1,2 +1,9 @@
 import requests
 
+
+def quest():
+    Complete_link = "https://bored-api.appbrewery.com/random"
+    response = requests.get(Complete_link)
+    return response.json()['activity']
+
+print(quest())
